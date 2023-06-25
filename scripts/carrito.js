cd24d50a713486d1effb7b0673deb68542fe486e
albegosu <albegosu@gmail.com>
2023-06-26 00:33:32 +0200

    Destruccion

---
+++ scripts/carrito.js
@@ -0,0 +1,48 @@
+export class Carrito {
+    #nombre;
+    productos;
+    unidades;
+    totalPrice;
+
+    constructor(nombre) {
+        this.#nombre = nombre;
+        this.productos = [];
+        this.unidades = 0;
+        this.totalPrice = 0;
+    }
+  
+    actualizarUnidades() {
+        let sum = 0;
+        this.productos.forEach(producto => {
+          sum += parseFloat(producto.cantidad);
+        });
+        this.unidades = sum;
+    }
+
+    sumaTotal() {
+        let sumTotal = 0;
+        this.productos.forEach(producto => {
+            sumTotal += parseFloat(producto.cantidad) * parseFloat(producto.price);
+        });
+        this.totalPrice = sumTotal;
+    }
+  
+    obtenerCarrito() {
+        const carritoDetallado = [];
+      
+        this.productos.forEach(producto => {
+          if (producto.cantidad >= 1) {
+            const { title, cantidad, price } = producto;
+            const subtotal = cantidad * price;
+            carritoDetallado.push({ title, cantidad, price, subtotal });
+          }
+        });
+      
+        const total = carritoDetallado.reduce((acc, { cantidad, price }) => acc + cantidad * price, 0);
+      
+        return {
+          total: total.toFixed(2),
+          carritoDetallado
+        };
+      }
+  }
---
+++ scripts/index.js
@@ -0,0 +1,134 @@
+import { Carrito } from "./carrito.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+const carrito = new Carrito(['AgoodShop Carrito']);
+
+fetch('https://jsonblob.com/api/jsonBlob/1122506093036322816')
+  .then(response => response.json())
+  .then(data => {
+    carrito.productos = data.products;
+    
+    crearCarrito(carrito);
+  });
+
+  function crearCarrito(carrito) {
+    const listadoCarrito = document.getElementById('carrito');
+  
+    // Crear tabla
+    const table = document.createElement('table');
+    table.classList.add('table');
+  
+    // Crear encabezados de columna
+    const thead = document.createElement('thead');
+    const headerRow = document.createElement('tr');
+    headerRow.classList.add('table__head');
+    headerRow.innerHTML = `<th class="table__head--title">Producto</th>
+    <th class="table__head--title">Cantidad</th>
+    <th class="table__head--title">Unidad</th>
+    <th class="table__head--title">Total</th>`;
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
+  
+    // Crear filas de productos
+    const tbody = document.createElement('tbody');
+    tbody.classList.add('table__body')
+    carrito.productos.forEach(producto => {
+      producto.cantidad = 0;
+      const row = document.createElement('tr');
+      row.classList.add('table__body--row')
+      row.innerHTML = `
+        <td><span class="table__productTitle">${producto.title}</span><br><span class="table__productRef">Ref: ${producto.SKU}</span></td>
+        <td class="input">
+          <div class="quantity">
+            <button class="quantity__button minus">-</button>
+            <input type="number" min="0" value="0" data-sku="${producto.SKU}" class="quantity__value" placeholder="0">
+            <button class="quantity__button plus">+</button>
+          </div>
+        </td>
+        <td class="price">${producto.price}€</td>
+        <td class="totalPrice" id="total-${producto.SKU}">0.00€</td>
+      `;
+      
+
+      const inputNum = row.querySelector('input');
+      const minusButton = row.querySelector('.minus');
+      const plusButton = row.querySelector('.plus');
+  
+      minusButton.addEventListener('click', () => {
+        const currentValue = parseInt(inputNum.value);
+        if (currentValue > 0) {
+          inputNum.value = currentValue - 1;
+
+          const quantityNow = inputNum.value;
+          actualizarTotal(quantityNow, producto);
+        } 
+      })
+      
+      plusButton.addEventListener('click', () => {
+        const currentValue = parseInt(inputNum.value);
+        inputNum.value = currentValue + 1;
+
+        const quantityNow = inputNum.value;
+        actualizarTotal(quantityNow, producto);
+      })
+      
+      inputNum.addEventListener('input', () => {
+        const quantityNow = parseInt(inputNum.value);
+        actualizarTotal(quantityNow, producto);
+      });
+
+      tbody.appendChild(row);
+    });
+
+    table.appendChild(tbody);
+    listadoCarrito.appendChild(table);
+  }
+  
+  function actualizarTotal(quantityNow, producto) {
+    const price = parseFloat(producto.price);
+    const total = quantityNow * price;
+  
+    const totalProducto = document.getElementById(`total-${producto.SKU}`);
+    totalProducto.innerText = `${total.toFixed(2)}€`;
+    
+    producto.cantidad = quantityNow;
+
+    //METODOS CARRITO
+    carrito.actualizarUnidades();
+    carrito.sumaTotal();
+
+    imprimirCarrito();
+
+    // CONSULTA CARRITO:
+    // console.log(carrito);
+  }
+
+  function imprimirCarrito() {
+    const totalCarrito = document.getElementById('total');
+    const resumenCarrito = document.getElementById('resumen');
+  
+    const { total, carritoDetallado } = carrito.obtenerCarrito();
+
+    resumenCarrito.innerHTML = '';
+  
+    carritoDetallado.forEach(producto => {
+      const productoGroup = document.createElement('article');
+      productoGroup.classList.add('resumen__producto');
+
+      const productoElement = document.createElement('p');
+      productoElement.innerText = producto.title;
+
+      const productoPrecio = document.createElement('p');
+      productoPrecio.innerText = `${producto.subtotal.toFixed(2)} €`;
+
+      productoGroup.appendChild(productoElement);
+      productoGroup.appendChild(productoPrecio);
+      resumenCarrito.appendChild(productoGroup);
+    });
+
+    totalCarrito.innerHTML = `<span class="totalAll">TOTAL</span><span class="blackPrice">${total} €</span>`;
+  }
+
+  imprimirCarrito();
+})
